d80beef1a7dccef46faee10529698abe5a390dfe
Francisco Anizio de Souza Junior <Francisco.Junior@caixavidaeprevidencia.com.br>
2022-06-25 10:54:23 -0300

    Fix .gitignore

--- src/main/java/br/com/franciscoanizio/App.java
+++ src/main/java/br/com/franciscoanizio/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Design Patterns");
+        System.out.println( "Design Patterns\n\n\n");
+
+        System.out.println( "\tCreational");
+        System.out.println( "\tStructural");
+        System.out.println( "\tBehavioral");
     }
 }
